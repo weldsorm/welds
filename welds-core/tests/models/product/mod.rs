df62f73b21f510ea17311123d50f7f8a0bcf9354
Lex Childs <lexchilds@gmail.com>
2023-02-19 08:26:56 -0600

    cleanup

--- welds-core/tests/models/product/mod.rs
+++ welds-core/tests/models/product/mod.rs
@@ -78,30 +78,3 @@ impl Product {
         select.where_col(lam)
     }
 }
-
-#[test]
-fn should_be_able_to_read_all_products() {
-    async_std::task::block_on(async {
-        let conn = testlib::postgres::conn().await.unwrap();
-        let pool: welds_core::database::Pool = conn.into();
-        let all = Product::all().run(&pool).await.unwrap();
-        assert_eq!(all.len(), 6, "Unexpected number of rows returned");
-    })
-}
-
-#[test]
-fn should_be_able_to_filter_on_equal() {
-    async_std::task::block_on(async {
-        let conn = testlib::postgres::conn().await.unwrap();
-        let pool: welds_core::database::Pool = conn.into();
-        let just_horse = Product::where_col(|x| x.price1.equal(1.10))
-            .run(&pool)
-            .await
-            .unwrap();
-        assert_eq!(
-            just_horse.len(),
-            1,
-            "Expected to only find the horse in the test data"
-        );
-    })
-}

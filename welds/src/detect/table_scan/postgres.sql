02970eaf178c6650dc72c513679577497b2197b2
Cajun <zac@kleinpeter.org>
2023-04-23 06:28:33 -0500

    formatting: reformatting the postgres.sql file
    - Breaking out sections into CTE to make the code more readable
    - Using sqlfluff to resolve SQL formatting issues and best practices

--- welds/src/detect/table_scan/postgres.sql
+++ welds/src/detect/table_scan/postgres.sql
@@ -1,31 +1,56 @@
+-- Pull the Primary keys from all tables
+WITH primary_keys AS (
+    SELECT 1
+    FROM information_schema.table_constraints AS tc
+    INNER JOIN information_schema.constraint_column_usage AS ccu
+        ON
+            tc.constraint_schema = ccu.constraint_schema
+            AND tc.constraint_name = ccu.constraint_name
+    WHERE tc.constraint_type = 'PRIMARY KEY'
+),
 
-SELECT 
-    things.schemaname as schema,
-    things.tablename as table_name,
+-- Create a CTE which contains all tables and views which need to be mapped
+tables_and_views AS (
+    SELECT
+        schemaname,
+        tablename,
+        'table' AS ty
+    FROM pg_catalog.pg_tables
+    WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema'
+
+    UNION
+
+    SELECT
+        table_schema AS schemaname,
+        table_name AS tablename,
+        'view' AS ty
+    FROM information_schema.views
+    WHERE table_schema = ANY(CURRENT_SCHEMAS(false))
+
+)
+
+SELECT
+    things.schemaname AS schema,
+    things.tablename AS table_name,
     things.ty,
     col.column_name,
-    col.udt_name as column_type,
-    case when col.is_nullable = 'YES' then 1 else 0 end as is_nullable,
-    case when (
-SELECT 1  
-FROM information_schema.table_constraints AS tc
-INNER JOIN
-    information_schema.constraint_column_usage AS ccu
+    col.udt_name AS column_type,
+    CASE WHEN col.is_nullable = 'YES' THEN 1 ELSE 0 END AS is_nullable,
+    CASE WHEN (
+        SELECT 1 FROM primary_keys AS keys
+        WHERE
+            keys.constraint_schema = things.schemaname
+            AND keys.table_name = things.tablename
+            AND keys.column_name = col.column_name
+    ) IS NOT NULL THEN 1 ELSE 0 END AS is_primary_key,
+    CASE WHEN col.is_updatable = 'YES' THEN 1 ELSE 0 END AS is_updatable
+FROM tables_and_views AS things
+INNER JOIN information_schema.columns AS col
     ON
-        tc.constraint_schema = ccu.constraint_schema
-        AND tc.constraint_name = ccu.constraint_name
-WHERE 
-    tc.constraint_type = 'PRIMARY KEY'
-    AND tc.constraint_schema = things.schemaname
-    AND tc.table_name = things.tablename
-    AND ccu.column_name = col.column_name
-
-) is not null then 1 else 0 end as is_primary_key,
-    case when col.is_updatable = 'YES' then 1 else 0 end as is_updatable
-FROM (
-    SELECT schemaname, tablename, 'table' as ty FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema'
-    UNION
-    SELECT table_schema as schemaname, table_name as tablename, 'view' as ty from INFORMATION_SCHEMA.views WHERE table_schema = ANY (current_schemas(false))
-) things
-join information_schema.columns col on col.table_schema = things.schemaname AND col.table_name = things.tablename
-ORDER BY things.schemaname, things.tablename, is_primary_key desc, col.column_name
+        col.table_schema = things.schemaname
+        AND col.table_name = things.tablename
+ORDER BY
+    things.schemaname ASC,
+    things.tablename ASC,
+    things.is_primary_key DESC,
+    col.column_name ASC

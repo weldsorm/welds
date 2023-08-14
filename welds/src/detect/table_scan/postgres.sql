4aa563901e597264edbbe729a7b42cf946c97e97
Lex Childs <lexchilds@gmail.com>
2023-08-14 13:25:22 -0500

    Fix view detection on other schemas

--- welds/src/detect/table_scan/postgres.sql
+++ welds/src/detect/table_scan/postgres.sql
@@ -23,9 +23,22 @@ WHERE
 ) is not null then 1 else 0 end as is_primary_key,
     case when col.is_updatable = 'YES' then 1 else 0 end as is_updatable
 FROM (
-    SELECT schemaname, tablename, 'table' as ty FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema'
+    SELECT schemaname, tablename, 'table' as ty FROM pg_catalog.pg_tables 
+      WHERE schemaname != 'pg_catalog' 
+      AND schemaname != 'information_schema'
+      AND schemaname != '_timescaledb_catalog'
+      AND schemaname != '_timescaledb_cache'
+      AND schemaname != '_timescaledb_config'
+      AND schemaname != '_timescaledb_internal'
+      AND schemaname != 'timescaledb_information'
+      AND schemaname != 'timescaledb_experimental'
     UNION
-    SELECT table_schema as schemaname, table_name as tablename, 'view' as ty from INFORMATION_SCHEMA.views WHERE table_schema = ANY (current_schemas(false))
+    SELECT table_schema as schemaname, table_name as tablename, 'view' as ty from INFORMATION_SCHEMA.views 
+      WHERE table_schema != 'pg_catalog'
+      AND table_schema != 'information_schema'
+      AND table_schema != '_timescaledb_internal'
+      AND table_schema != 'timescaledb_information'
+      AND table_schema != 'timescaledb_experimental'
 ) things
 join information_schema.columns col on col.table_schema = things.schemaname AND col.table_name = things.tablename
 ORDER BY things.schemaname, things.tablename, is_primary_key desc, col.column_name

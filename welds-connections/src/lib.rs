c8e7dcd067bedb58f9aeeead5acc5a7b62606101
liskaant <liskaant@gmail.com>
2025-01-17 23:56:54 +0100

    feat: add `ExecuteResult` constructor

--- welds-connections/src/lib.rs
+++ welds-connections/src/lib.rs
@@ -157,6 +157,10 @@ pub struct ExecuteResult {
 }
 
 impl ExecuteResult {
+    pub fn new(rows_affected: u64) -> Self {
+        Self { rows_affected }
+    }
+
     pub fn rows_affected(&self) -> u64 {
         self.rows_affected
     }

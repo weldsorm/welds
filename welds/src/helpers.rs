f0962d0f88b29620a277e0a935a6e69e0aff2892
retorted <201078195+retorted@users.noreply.github.com>
2025-03-04 12:47:23 +0000

    use slices and output models without DbState<_> wrappers

--- welds/src/helpers.rs
+++ welds/src/helpers.rs
@@ -2,7 +2,6 @@ use std::borrow::Borrow;
 use std::collections::HashMap;
 
 use crate::model_traits::{HasSchema, PkValue, TableColumns};
-use crate::prelude::DbState;
 use crate::relations::{BelongsTo, BelongsToFkValue, HasMany, Related};
 
 /// Combines two sets of related objects into a Vec of structs, by given lambda. Eg:
@@ -27,45 +26,45 @@ use crate::relations::{BelongsTo, BelongsToFkValue, HasMany, Related};
 /// }
 ///
 /// struct TeamWithPlayers {
-///     team: DbState<Team>,
-///     players: Vec<DbState<Player>>,
+///     team: Team,
+///     players: Vec<Player>,
 /// }
 ///
 /// fn teams_with_players(&dyn Client) -> Result<Vec<TeamWithPlayers>> {
-///     let all_teams = Team::all().run(&client).await?;
-///     let all_player = Player::all().run(&client).await?;
+///     let all_teams = Team::all().run(&client).await?.into_inners();
+///     let all_player = Player::all().run(&client).await?.into_inners();
 ///
 ///     let lambda = |team, players| { TeamWithPlayers { team, players } };
-///     let combined = combine_related(lambda, all_teams, all_players);
+///     let combined = combine_related(lambda, &all_teams, &all_players);
 ///
 ///     Ok(combined)
 /// }
 /// ```
 pub fn combine_related<OutputStruct, Parent, Child>(
-    lambda: impl Fn(DbState<Parent>, Vec<DbState<Child>>) -> OutputStruct,
-    parents: Vec<DbState<Parent>>,
-    children: Vec<DbState<Child>>,
+    lambda: impl Fn(Parent, Vec<Child>) -> OutputStruct,
+    parents: &[Parent],
+    children: &[Child],
 ) -> Vec<OutputStruct>
 where
-    Parent: HasSchema + Related<HasMany<Child>> + PkValue,
+    Parent: HasSchema + Related<HasMany<Child>> + PkValue + ToOwned<Owned = Parent>,
     <Parent as HasSchema>::Schema: TableColumns,
-    Child: HasSchema + Related<BelongsTo<Parent>> + BelongsToFkValue<Parent>,
+    Child: HasSchema + Related<BelongsTo<Parent>> + BelongsToFkValue<Parent> + ToOwned<Owned = Child>,
     <Child as HasSchema>::Schema: TableColumns,
     <Parent as PkValue>::PkVal: Borrow<<Child as BelongsToFkValue<Parent>>::FkVal>
 {
-    group_related(&parents, children)
+    group_related(parents, children)
         .into_iter()
         .zip(parents)
-        .map(|(children, parent)| lambda(parent, children))
+        .map(|(children, parent)| lambda(parent.to_owned(), children))
         .collect()
 }
 
 /// Groups a set of objects with a set of related objects for use with `.zip()`
-pub fn group_related<Parent, Child>(parents: &Vec<DbState<Parent>>, children: Vec<DbState<Child>>) -> Vec<Vec<DbState<Child>>>
+pub fn group_related<Parent, Child>(parents: &[Parent], children: &[Child]) -> Vec<Vec<Child>>
 where
     Parent: HasSchema + Related<HasMany<Child>> + PkValue,
     <Parent as HasSchema>::Schema: TableColumns,
-    Child: HasSchema + Related<BelongsTo<Parent>> + BelongsToFkValue<Parent>,
+    Child: HasSchema + Related<BelongsTo<Parent>> + BelongsToFkValue<Parent> + ToOwned<Owned = Child>,
     <Child as HasSchema>::Schema: TableColumns,
     <Parent as PkValue>::PkVal: Borrow<<Child as BelongsToFkValue<Parent>>::FkVal>
 {
@@ -81,7 +80,7 @@ where
 
     for child in children {
         if let Some(index) = indexed.get(&child.fk_value::<Parent>()) {
-            grouped[*index].push(child);
+            grouped[*index].push(child.to_owned());
         }
     }
 

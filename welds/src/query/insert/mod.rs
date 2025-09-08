35d307f8f4eb9ce6c9b75107f6f9c4ffecdafa86
Lex Childs <lexchilds@gmail.com>
2025-09-08 08:51:03 -0500

    move bulk insert from unstable to stable

--- welds/src/query/insert/mod.rs
+++ welds/src/query/insert/mod.rs
@@ -6,7 +6,5 @@ pub use single::insert_one;
 pub use bulk::bulk_insert;
 pub use bulk::bulk_insert_with_ids;
 
-#[cfg(feature = "unstable-api")]
 pub use bulk::bulk_insert_override_tablename_unsafe;
-#[cfg(feature = "unstable-api")]
 pub use bulk::bulk_insert_with_ids_override_tablename_unsafe;

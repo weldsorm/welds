3f2aac9faf557aa26b8086a0d2ea4d223da8dc1d
Lex Childs <lexchilds@gmail.com>
2026-03-24 14:41:08 -0500

    feat: added a way to have different manual migrations for differance syntax

--- welds/src/migrations/manual.rs
+++ welds/src/migrations/manual.rs
@@ -1,9 +1,14 @@
+use welds_connections::Syntax;
+
 use crate::migrations::MigrationWriter;
 use crate::migrations::utils::split_sql_commands;
+use std::collections::HashMap;
 
 pub struct Manual {
-    up: String,
-    down: String,
+    up_default: String,
+    down_default: String,
+    up_syntax: HashMap<Syntax, String>,
+    down_syntax: HashMap<Syntax, String>,
 }
 
 impl Manual {
@@ -11,25 +16,47 @@ impl Manual {
     /// NOTE: you can write multiple commands executing each after ';'
     pub fn up(sql: impl Into<String>) -> Manual {
         Manual {
-            up: sql.into(),
-            down: "".to_string(),
+            up_default: sql.into(),
+            down_default: "".to_string(),
+            up_syntax: HashMap::new(),
+            down_syntax: HashMap::new(),
         }
     }
 
     /// Write the down part of this manual migration.
     /// NOTE: you can write multiple commands executing each after ';'
     pub fn down(mut self, sql: impl Into<String>) -> Manual {
-        self.down = sql.into();
+        self.down_default = sql.into();
+        self
+    }
+
+    /// write some RAW for a specific Database syntax
+    /// NOTE: you can write multiple commands executing each after ';'
+    pub fn up_for(mut self, syntax: Syntax, sql: impl Into<String>) -> Manual {
+        self.up_syntax.insert(syntax, sql.into());
+        self
+    }
+
+    /// Write the down part of this manual migration for only a given syntax
+    /// NOTE: you can write multiple commands executing each after ';'
+    pub fn down_for(mut self, sql: impl Into<String>) -> Manual {
+        self.down_default = sql.into();
         self
     }
 }
 
 impl MigrationWriter for Manual {
-    fn up_sql(&self, _syntax: welds_connections::Syntax) -> Vec<String> {
-        split_sql_commands(&self.up)
+    fn up_sql(&self, syntax: welds_connections::Syntax) -> Vec<String> {
+        if let Some(sql) = self.up_syntax.get(&syntax) {
+            return split_sql_commands(sql);
+        }
+        split_sql_commands(&self.up_default)
     }
 
-    fn down_sql(&self, _syntax: welds_connections::Syntax) -> Vec<String> {
-        split_sql_commands(&self.down)
+    fn down_sql(&self, syntax: welds_connections::Syntax) -> Vec<String> {
+        if let Some(sql) = self.down_syntax.get(&syntax) {
+            return split_sql_commands(sql);
+        }
+        split_sql_commands(&self.down_default)
     }
 }
